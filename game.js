e07464091c5c747627723fe86116d856bfe3baa9
XtraHamster <martinkrecek98@seznam.cz>
2020-08-06 18:21:03 +0200

    Added Difficulty
    Added Easy and Hard buttons. User can play easy version with only 3 colors or hard version with 6 colors.

--- game.js
+++ game.js
@@ -1,16 +1,49 @@
 var colors = generateRandomColors(6);
 var squares = document.querySelectorAll('.square');
-var pickedColor = pickColors();
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+var numSquares = 6;
+
+easyBtn.addEventListener('click', function(){
+	hardBtn.classList.remove('selected');
+	easyBtn.classList.add('selected');
+	numSquares = 3;
+	colors = generateRandomColors(3);
+	pickedColor = pickColor()
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = 'none';
+		}
+	}
+});
+
+hardBtn.addEventListener('click', function(){
+	hardBtn.classList.add('selected');
+	easyBtn.classList.remove('selected');
+	numSquares = 6;
+	colors = generateRandomColors(6);
+	pickedColor = pickColor()
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+			squares[i].style.backgroundColor = colors[i];
+			squares[i].style.display = 'block';
+		}
+	}
+);
 
 resetButton.addEventListener('click', function(){
 	// generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	// pick a new random color from a array
-	pickedColor = pickColors()
+	pickedColor = pickColor()
 	// change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
 	// change colors of squares
@@ -52,7 +85,7 @@ function changeColor(color) {
 	}
 }
 
-function pickColors() {
+function pickColor() {
 	var random = Math.floor(Math.random() * colors.length)
 	return colors[random];
 }
