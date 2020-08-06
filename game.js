c0c0b02e7e42aeab177587f6b923dab716f20a1f
XtraHamster <martinkrecek98@seznam.cz>
2020-08-06 11:21:42 +0200

    Color funcionality
    Gme works, random rgb color shows up in header and user have to click on color square, If he guesses wrong, square disappears and can try again. If user guesses correct, all squares changes color to the correct one.

--- game.js
+++ game.js
@@ -8,20 +8,41 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+	// add initial colors to squares
 	squares[i].style.backgroundColor = colors[i]
 
+	// add click listeners to squares
 	squares[i].addEventListener('click', function() {
+		// grab color of clicked square
 		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
-			alert('Correct');
-		} else {
-			alert('Wrong');
+			messageDisplay.textContent = 'Correct';
+			changeColor(clickedColor);
+		} 
+		else {
+			this.style.backgroundColor = '#232323';
+			messageDisplay.textContent = 'Try Again';
 		}
 	});
+}
+
+function changeColor(color) {
+	// loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		// change each color to match given color
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length)
+	return colors[random];
 }
